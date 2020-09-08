3be77bcce9dcd05eb2b27fbe85e63bb2de948dc2
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-09-08 15:58:01 -0400

    Update FixNetShare.ps1

--- Fixers/FixNetShare.ps1
+++ Fixers/FixNetShare.ps1
@@ -1,11 +1,16 @@
-#Turns on network discovery and dependent services
-Set-Service -Name FDResPub -StartupType Automatic
-Set-Service -Name SSDPSRV -StartupType Automatic
-Set-Service -Name upnphost -StartupType Automatic
-Set-Service -Name dnscache -StartupType Automatic
-Start-Service FDResPub
-Start-Service SSDPSRV
-Start-Service upnphost
-Start-Service Dnscache
+#Elsewhen Technology Scripts
+#Author: Nic "Rant" Colon
+#FixNetShare 
+#This script, once started, perpetually checks to ensure that the services network and resource sharing are dependent on are in fact running and enabled.
 netsh advfirewall firewall set rule group="network discovery" new enable=yes
 netsh firewall set service type=fileandprint mode=enable profile=all
+[Array] $Servs = 'fdrespub','ssdpsrv','upnphost','dnscache'
+$Keystone=0
+while($Keystone = 0)
+{
+    foreach($ServiceName in $Servs)
+    {
+        Get-Service $ServiceName | Where {$_.Status -eq 'Stopped'} | Start-Service 
+    }
+    Start-Sleep -s 1800
+}
