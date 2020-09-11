90f6b33d053a812151d0dc58a90ceffc510b095e
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-09-11 11:30:25 -0400

    Update ScheduleNightReboot.ps1

--- Technician Tools/ScheduleNightReboot.ps1
+++ Technician Tools/ScheduleNightReboot.ps1
@@ -1,2 +1,6 @@
-﻿$Date = Get-Date -Format "MM/dd/yyyy"
+#Elsewhen Techology Scripts
+#Author: Nic "Rant" Colon
+#Last Update: 09/11/2020
+#This script scheduled the target device for a reboot for midnight of whatever day the script is run on. 
+$Date = Get-Date -Format "MM/dd/yyyy"
 cmd.exe | schtasks /create /tn "Scheduled Reboot" /tr "shutdown /r t/ 0" /sc once /st 00:00:00 /sd $_.Date /ru "System"
