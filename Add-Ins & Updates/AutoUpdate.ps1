d7faecd318b8b9347c7dc5dc74eec6ebbb559302
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-09-11 10:57:30 -0400

    Update AutoUpdate.ps1

--- Add-Ins & Updates/AutoUpdate.ps1
+++ Add-Ins & Updates/AutoUpdate.ps1
@@ -1,7 +1,7 @@
 ﻿#AutoUpdate.ps1
 #Elsewhen Technology Automation Script
 #Author: Nic "Rant" Colon
-#Last Updated: 08/17/2020
+#Last Updated: 09/11/2020
 #This script is for scheduled tasks, it automatically checks for, downloads, and installs updates once a week without user input
 Get-Windowsupdate -microsoftupdate -ForceDownload -ForceInstall -acceptall -confirm:$false
 Download-WindowsUpdate -MicrosoftUpdate -AcceptAll -Confirm:$False
