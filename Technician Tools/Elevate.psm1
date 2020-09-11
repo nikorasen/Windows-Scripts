4f9505d39edb3347b4dc13fe3cbdbbdd4d474817
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-09-11 15:43:27 -0400

    Add files via upload
    Placing this script within its own named folder within the path: C:\Windows\System32\WindowsPowershell\v1.0\Modules allows one to elevate to an admin command prompt by entering the command "Elevate" into powershell.

---
+++ Technician Tools/Elevate.psm1
@@ -0,0 +1,25 @@
+﻿Function Elevate
+    {
+    $ver = $host | select version
+    if ($ver.Version.Major -gt 1)  {$Host.Runspace.ThreadOptions = "ReuseThread"}
+#
+# Verify that user running script is an administrator
+    $IsAdmin=[Security.Principal.WindowsIdentity]::GetCurrent()
+    If ((New-Object Security.Principal.WindowsPrincipal $IsAdmin).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE)
+        {
+        "`nERROR: You are NOT a local administrator.  Run this script after logging on with a local administrator account."
+            # We are not running "as Administrator" - so relaunch as administrator
+
+            # Create a new process object that starts PowerShell
+            $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell.exe";
+
+            # Indicate that the process should be elevated
+            $newProcess.Verb = "runas";
+
+            # Start the new process
+            [System.Diagnostics.Process]::Start($newProcess);
+
+            # Exit from the current, unelevated, process
+            exit
+        }
+    }
