a624cae24f3bee7046369e6a9bfdea6e39016035
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-10-06 09:56:32 -0400

    Add files via upload

---
+++ Add-Ins & Updates/CreateAutoUpdateTask.ps1
@@ -0,0 +1,17 @@
+﻿#Schedules Scripted Maintenance Tasks
+Function SchTask1
+    {
+    $User = "NT AUTHORITY\SYSTEM"
+    $Script = "C:\EWT\Scripts\AutoUpdate.ps1" #edit at scripter discretion, able to schedule any script to run
+    $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $Script
+    $Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Saturday -At 1am
+    Register-ScheduledTask -TaskName "Automatic Update Weekly" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+    }
+Function SchTask2
+    {
+    $User = "NT AUTHORITY\SYSTEM"
+    $Script = "C:\EWT\Scripts\AutoUpdate.ps1" #edit at scripter discretion, able to schedule any script to run
+    $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $Script
+    $Trigger = New-ScheduledTaskTrigger -Once -At 1am
+    Register-ScheduledTask -TaskName "Automatic Update Weekly" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+    }
