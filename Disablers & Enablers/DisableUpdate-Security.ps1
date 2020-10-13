78531b6e959a704746733665f759de178729c893
nikorasen <48002898+nikorasen@users.noreply.github.com>
2020-10-13 16:52:35 -0400

    Add files via upload
    This one murders Windows Update and Defender with a rusty spoon.

---
+++ Disablers & Enablers/DisableUpdate-Security.ps1
@@ -0,0 +1,21 @@
+﻿#Rant
+#IS
+#KING
+#This script fucks windows updates into oblivion
+#
+[Array] $Servs = 'wuauserv', 'SecurityHealthService', 'Sense', 'WdNisSvc', 'WinDefend'
+#Looper variable, helps the script run perpetually
+$Looper = 0
+while($Looper = 0)
+{
+    foreach($ServiceName in $Servs)
+    {
+        Get-Service $ServiceName | Where {$_.Status -eq 'Running'} | Stop-Service
+        Set-Service $ServiceName -StartupType Disabled 
+    }
+    sc stop WinDefend 
+    sc config WinDefend start = disabled
+    Set-MpPreference -DisableRealtimeMonitoring $true
+    Uninstall-WindowsFeature -Name Windows-Defender
+    Start-Sleep -s 1800
+}
